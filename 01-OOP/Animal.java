c091f4defce782f07dc82ad1e127003db72d9a31
Prathyusha Manne <prathyushamanne914@gmail.com>
2026-08-24 16:20:21 +0530

    Add files via upload

---
+++ 01-OOP/Animal.java
@@ -0,0 +1,18 @@
+package com.te.inheritance;
+
+public class Animal {
+      
+      String name;
+      String color;
+      
+      
+      public void eat()
+      {
+          System.out.println("The animal is eating");
+      }
+      
+      public void sleep() {
+          System.out.println("The animal is Sleeping");
+      }
+      
+}
